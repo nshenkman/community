fbb4dee8db6c3fb83a8eded9c32a48a863fa95b7
nikita.shenkman <nikita.shenkman@gmail.com>
2025-04-13 03:22:02 -0700

    one more

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 #/go/src/github.com/tidbyt/pixlet/pixlet render /whistle.star email=$3 password=$4 pet=$5 && /go/src/github.com/tidbyt/pixlet/pixlet push --api-token $1 --installation-id Whistle $2 -b /whistle.webp
 /go/src/github.com/tidbyt/pixlet/pixlet render /fi.star email=$6 password=$7 pet=$8 && /go/src/github.com/tidbyt/pixlet/pixlet push --api-token $1 --installation-id Fi $2 -b /fi.webp
-/go/src/github.com/tidbyt/pixlet/pixlet render /usemotion.star apiKey=$9 && /go/src/github.com/tidbyt/pixlet/pixlet push --api-token $1 --installation-id Fi $2 -b /usemotion.webp
+/go/src/github.com/tidbyt/pixlet/pixlet render /usemotion.star apiKey=$9 && /go/src/github.com/tidbyt/pixlet/pixlet push --api-token $1 --installation-id useMotion $2 -b /usemotion.webp
